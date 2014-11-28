fd35a972de9b257c728bc73f84fc50db9191d9bb
sanketjpatel <sanketpatel.301090@gmail.com>
2014-11-28 00:16:40 -0500

    Comment

--- 04_ConstructorDestructor.cpp
+++ 04_ConstructorDestructor.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-
-using namespace std;
-
-class tester {
-	static int count;
-public:
-	tester(){
-		count++;
-		cout << "\nNo. of objects created = " << count;
-	}
-	~tester(){
-		count--;
-		cout << "\nNo. of objects left = " << count;
-	}
-};
-
-int tester::count;
-
-int main() {
-	cout << "\n\nENTER MAIN\n";
-	tester a, b, c, d;
-	{
-		cout << "\n\nENTER BLOCK1\n";
-		tester e;
-	}
-
-	{
-		cout << "\n\nENTER BLOCK2\n";
-		tester f;
-	}
-
-	cout << "\n\nRE-ENTER MAIN\n";
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//class tester {
+//	static int count;
+//public:
+//	tester(){
+//		count++;
+//		cout << "\nNo. of objects created = " << count;
+//	}
+//	~tester(){
+//		count--;
+//		cout << "\nNo. of objects left = " << count;
+//	}
+//};
+//
+//int tester::count;
+//
+//int main() {
+//	cout << "\n\nENTER MAIN\n";
+//	tester a, b, c, d;
+//	{
+//		cout << "\n\nENTER BLOCK1\n";
+//		tester e;
+//	}
+//
+//	{
+//		cout << "\n\nENTER BLOCK2\n";
+//		tester f;
+//	}
+//
+//	cout << "\n\nRE-ENTER MAIN\n";
+//
+//	return 0;
+//}
