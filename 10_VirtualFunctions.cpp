b58ef42f6a45e203092cfbb9d7ddd4c2608eba52
sanketjpatel <sanketpatel.301090@gmail.com>
2014-11-30 19:49:20 -0500

    Add destructor and comments

--- 10_VirtualFunctions.cpp
+++ 10_VirtualFunctions.cpp
@@ -6,12 +6,23 @@ class shape{
 protected:
 	double x, y;
 public:
+	/*
+	 * When a method is made virtual,
+	 * the pointer will invoke the method in the derived class.
+	 */
 	virtual void get_data(){};
+
+	// Function overloading
 	void get_data(double primary, double secondary){
 		x = primary;
 		y = secondary;
 	}
 	virtual void display_area(){};
+
+	// Destructor is made virtual so that destructor in the derived class is invoked
+	virtual ~shape(){
+		cout << "Destroying shape with dimensions: " << x << ", " << y << endl;
+	}
 };
 
 class triangle : public shape{
@@ -27,6 +38,10 @@ public:
 
 	void display_area(){
 		cout << "Area of triangle = " << x*y/2 << endl;
+	}
+
+	~triangle(){
+		cout << "\nDeleted triangle" << endl;
 	}
 };
 
@@ -44,6 +59,10 @@ public:
 	void display_area(){
 		cout << "Area of rectangle = " << x*y << endl;
 	}
+
+	~rectangle(){
+		cout << "\nDeleted rectangle" << endl;
+	}
 };
 
 void VirtualFunctions(){
@@ -52,7 +71,6 @@ void VirtualFunctions(){
 	cin >> n;
 
 	shape* shapes[n];
-//	double primary, secondary;
 
 	int shapetype;
 
@@ -63,17 +81,20 @@ void VirtualFunctions(){
 		if (shapetype == 1) shapes[i] = new triangle;
 		else shapes[i] = new rectangle;
 
-//		cout << "Enter primary dimension: ";
-//		cin >> primary;
-//		cout << "Enter secondary dimension: ";
-//		cin >> secondary;
 		shapes[i] -> get_data();
-//		shapes[i] -> display_area();
 	}
 
 	for (int i = 0; i<n; i++){
-		cout << "\n******* Shape " << i << " *******" << endl;
+		cout << "\n******* Shape " << i << ": " << shapes[i] <<" *******" << endl;
 		shapes[i] -> display_area();
+	}
+
+	for (int i=n-1; i>=0; i--){
+		/* This invokes the destructor of the derived class followed by the destructor of the base class,
+		 * since the destructor in the base class has been defined as virtual.
+		 * If the destructor in the base class is not virtual, then the derived class destructor is not invoked.
+		 */
+		delete shapes[i];
 	}
 
 }
